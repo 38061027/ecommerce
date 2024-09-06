4c40a6bce033117d023526089f69ad9eacfe0b86
Matheus <matheus.fernandes-movida@outlook.com>
2024-09-06 17:29:37 -0300

    Adicionando elementos do material

--- front-end/src/app/material/material/material.module.ts
+++ front-end/src/app/material/material/material.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
-
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+const MODULES = [
+  MatSelectModule,
+  MatInputModule,
+  MatFormFieldModule
+]
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MODULES
+  ],
+  exports:[MODULES]
 })
 export class MaterialModule { }
