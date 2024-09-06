cbc159864f6eceff910818b5f2a32b7bbb38bc2e
Matheus <matheus.fernandes-movida@outlook.com>
2024-09-06 14:29:38 -0300

    components criados

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { DetailsComponent } from './components/details/details.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailsComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
