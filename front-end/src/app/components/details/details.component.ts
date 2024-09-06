9dd4119feb74f3e3bc8d8fd4825ddf3730fee75d
Matheus <matheus.fernandes-movida@outlook.com>
2024-09-06 17:30:27 -0300

    Criando o componente de details

--- front-end/src/app/components/details/details.component.ts
+++ front-end/src/app/components/details/details.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProducts } from 'src/app/interface/interface.';
+import { SharedService } from 'src/app/service/shared.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit{
+id!:number | null;
+product!:IProducts | undefined;
+constructor(private activatedRoute: ActivatedRoute,private service: SharedService){}
+
+ngOnInit(): void {
+  this.id = Number(this.activatedRoute.snapshot.paramMap.get("id"));
+  if(this.id){
+    this.service.getProducts().subscribe((product:IProducts[])=>{
+      const p = product.find(p=>p.id == this.id)
+      this.product = p
+    })
+  }
+  console.log(this.id)
+}
 
 }
