7b2e076cb06ec3027c81e7a860da021e3c12916f
Matheus <matheus.fernandes-movida@outlook.com>
2024-09-06 17:31:03 -0300

    Criando requisições

--- front-end/src/app/service/shared.service.ts
+++ front-end/src/app/service/shared.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IProducts } from '../interface/interface.';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
+  apiUrl = "http://localhost:3000/api/data"
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+
+  getProducts():Observable<IProducts[]>{
+    return this.http.get<IProducts[]>(this.apiUrl)
+  }
+
+  sendProducts(product:IProducts):Observable<IProducts>{
+    return this.http.post<IProducts>(`${this.apiUrl}`, product)
+  }
+
 }
