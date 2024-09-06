677233db4180e29769ba5cc4b5910e05373f9d49
Matheus <matheus.fernandes-movida@outlook.com>
2024-09-06 15:07:24 -0300

    Criando base do projeto

--- front-end/src/app/components/home/home.component.ts
+++ front-end/src/app/components/home/home.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  counterCart:number = 2;
+
+  products:any[] = [
+    {
+      "id": 1,
+      "name": "Apple iPhone 15",
+      "quantity": 5,
+      "description": "O iPhone 15 traz a Dynamic Island, câmera grande-angular de 48 MP e USB-C. Tudo em um vidro resistente colorido por infusão e design em alumínio.",
+      "price": "4.750",
+      "status": 1
+    },
+    {
+      "id": 2,
+      "name": "Apple iPhone 10",
+      "quantity": 3,
+      "description": "O iPhone 15 traz a Dynamic Island, câmera grande-angular de 48 MP e USB-C. Tudo em um vidro resistente colorido por infusão e design em alumínio.",
+      "price": "4.750",
+      "status": 1
+    },
+    {
+      "id": 3,
+      "name": "Apple iPhone 11",
+      "quantity": 1,
+      "description": "O iPhone 15 traz a Dynamic Island, câmera grande-angular de 48 MP e USB-C. Tudo em um vidro resistente colorido por infusão e design em alumínio.",
+      "price": "4.750",
+      "status": 0
+    }
+  ]
+  
 }
