a071e2d13ee22ae10c5df8ecc93cf72d90c3abce
Matheus <matheus.fernandes-movida@outlook.com>
2024-09-06 17:29:58 -0300

    Criando rotas

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { FormComponent } from './components/form/form.component';
+import { DetailsComponent } from './components/details/details.component';
 
 const routes: Routes = [
-  {path:"", component: HomeComponent, pathMatch:"full"}
+  {path:"", component: HomeComponent, pathMatch:"full"},
+  {path:"form", component: FormComponent},
+  {path:"details/:id", component: DetailsComponent},
 ];
 
 @NgModule({
