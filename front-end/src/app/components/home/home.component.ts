4d71775839975dc511acfa2158148dec25362c31
Matheus <matheus.fernandes-movida@outlook.com>
2024-09-06 17:32:01 -0300

    Estrutura da home criada

--- front-end/src/app/components/home/home.component.ts
+++ front-end/src/app/components/home/home.component.ts
@@ -1,39 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProducts } from 'src/app/interface/interface.';
+import { SharedService } from 'src/app/service/shared.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
   counterCart:number = 2;
+  products!:IProducts[];
 
-  products:any[] = [
-    {
-      "id": 1,
-      "name": "Apple iPhone 15",
-      "quantity": 5,
-      "description": "O iPhone 15 traz a Dynamic Island, câmera grande-angular de 48 MP e USB-C. Tudo em um vidro resistente colorido por infusão e design em alumínio.",
-      "price": "4.750",
-      "status": 1
-    },
-    {
-      "id": 2,
-      "name": "Apple iPhone 10",
-      "quantity": 3,
-      "description": "O iPhone 15 traz a Dynamic Island, câmera grande-angular de 48 MP e USB-C. Tudo em um vidro resistente colorido por infusão e design em alumínio.",
-      "price": "4.750",
-      "status": 1
-    },
-    {
-      "id": 3,
-      "name": "Apple iPhone 11",
-      "quantity": 1,
-      "description": "O iPhone 15 traz a Dynamic Island, câmera grande-angular de 48 MP e USB-C. Tudo em um vidro resistente colorido por infusão e design em alumínio.",
-      "price": "4.750",
-      "status": 0
-    }
-  ]
+
+constructor(private service: SharedService){}
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+  getProducts(){
+    return this.service.getProducts().subscribe(res => this.products = res)
+  }
   
 }
