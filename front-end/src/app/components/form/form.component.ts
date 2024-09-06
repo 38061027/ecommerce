6c4f1436d70d1f21f468deb3779d99419167fb2c
Matheus <matheus.fernandes-movida@outlook.com>
2024-09-06 17:30:46 -0300

    Criando formulario

---
+++ front-end/src/app/components/form/form.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { IProducts } from 'src/app/interface/interface.';
+import { SharedService } from 'src/app/service/shared.service';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent {
+
+  productRegister:FormGroup;
+
+    constructor(private service:SharedService, private fb: FormBuilder){
+      this.productRegister = this.fb.group({
+        name:['',Validators.required],
+        quantity:['',Validators.required],
+        description:['',Validators.required],
+        price:['',Validators.required],
+        status:['',Validators.required]
+      })
+    }
+
+    sendProduct(){
+      if(this.productRegister.valid){
+        const product = this.productRegister.value;
+        console.log(product)
+        this.service.sendProducts(product).subscribe()
+      }
+    }
+
+}
