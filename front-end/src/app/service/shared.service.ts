b1237c7fa556870e6cebcb927bd48c0236ffe67c
38061027 <joseaugustogomessantos12@gmail.com>
2024-09-08 18:37:40 -0300

    Alterando quantidade do produto

--- front-end/src/app/service/shared.service.ts
+++ front-end/src/app/service/shared.service.ts
@@ -1,22 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { IProducts } from '../interface/interface.';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SharedService {
-  apiUrl = "http://localhost:3000/api/data"
-  constructor(private http: HttpClient) { }
+  apiUrl: string = 'http://localhost:3000/api/data';
+  urlJsonServer: string = 'http://localhost:3000/products';
 
+  constructor(private http: HttpClient) {}
 
-  getProducts():Observable<IProducts[]>{
-    return this.http.get<IProducts[]>(this.apiUrl)
+  getProducts(): Observable<IProducts[]> {
+    return this.http.get<IProducts[]>(this.urlJsonServer);
   }
 
-  sendProducts(product:IProducts):Observable<IProducts>{
-    return this.http.post<IProducts>(`${this.apiUrl}`, product)
+  sendProducts(product: IProducts): Observable<IProducts> {
+    return this.http.post<IProducts>(`${this.urlJsonServer}`, product);
   }
 
+  getCart(): Observable<IProducts[]> {
+    return this.http.get<IProducts[]>('http://localhost:3000/cart');
+  }
+
+  updateQuantity(id: number, newQuantity: number): Observable<any> {
+    return this.http.patch(`http://localhost:3000/cart/${id}`, { quantity: newQuantity });
+  }
+  
 }
