021a70f48a3726db0fac27934570cd70daf2dc65
Matheus <matheus.fernandes-movida@outlook.com>
2024-09-10 17:49:33 -0300

    Ajustes no home

--- front-end/src/app/components/home/home.component.ts
+++ front-end/src/app/components/home/home.component.ts
@@ -12,12 +12,16 @@ export class HomeComponent implements OnInit {
   products!: IProducts[];
   existingMsg: boolean = false;
   insertToCart: boolean = false;
+  active:boolean = false;
 
   constructor(private service: SharedService) {}
 
   ngOnInit(): void {
     this.getProducts();
     this.service.getCart().subscribe((res) => (this.counterCart = res.length));
+  }
+  cartActive(){
+    this.active = true
   }
   getProducts() {
     return this.service.getProducts().subscribe((res) => (this.products = res));
@@ -34,6 +38,7 @@ export class HomeComponent implements OnInit {
       } else {
         product.quantity = 1;
         this.service.sendToCart(product).subscribe(() => {
+          this.counterCart++;
           this.insertToCart = true;
           setTimeout(() => {
             this.insertToCart = false;
