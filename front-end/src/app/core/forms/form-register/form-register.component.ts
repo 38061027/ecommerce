124d98728d0f73d84ca68f1756e0e470a1db8db8
Matheus <matheus.fernandes-movida@outlook.com>
2024-09-13 14:48:28 -0300

    Criando o formulario

--- front-end/src/app/core/forms/form-register/form-register.component.ts
+++ front-end/src/app/core/forms/form-register/form-register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SharedService } from 'src/app/service/shared.service';
 
 @Component({
   selector: 'app-form-register',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./form-register.component.css']
 })
 export class FormRegisterComponent {
+
+  registerUser:FormGroup
+
+    constructor(private fb: FormBuilder, private service: SharedService){
+      this.registerUser = fb.group({
+        name: ['', Validators.minLength(4)],
+        hierarchy: ['', Validators.minLength(4)],
+        email: ['', Validators.minLength(5)],
+        password: ['', Validators.minLength(6)],
+      });
+    }
+
+    onRegister(){
+      if(this.registerUser.valid){
+        const newUser = this.registerUser.value
+        this.service.registerUser(newUser).subscribe()
+      }
+    }
+
 
 }
