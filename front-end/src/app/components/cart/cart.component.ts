33e0c2c4858f9bc682169fd1d70af409d40eba45
José Augusto <joseaugustogomessantos12@gmail.com>
2024-09-28 12:08:38 -0300

    Ajustes no cart

--- front-end/src/app/components/cart/cart.component.ts
+++ front-end/src/app/components/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { IProducts } from 'src/app/core/interface/interface.';
+import { Icart, IProducts, IUsers } from 'src/app/core/interface/interface.';
 import { CartService } from 'src/app/services/cart/cart.service';
+import { UsersService } from 'src/app/services/users/users.service';
 
 
 @Component({
@@ -9,32 +10,35 @@ import { CartService } from 'src/app/services/cart/cart.service';
   styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
-  cart!: IProducts[];
+  cart: Icart[] = [];
   totalValue:number = 0;
-  constructor(private cartService: CartService) {}
+  constructor(private usersService: UsersService) {}
 
   ngOnInit(): void {
     this.getCart();
   }
 
   getCart() {
-    this.cartService.getCart().subscribe((res: IProducts[]) => {
-      this.cart = res,
-      this.totalValue = res.reduce((a,b)=>a+b.price,0)
+    this.usersService.getUser().subscribe((res: IUsers[]) => {
+      let productsInCart = res.map(el => el.cart)
+      console.log(productsInCart)
+      // this.cart.push()
+      // this.totalValue = res.reduce((a,b)=>a+b.price*b.quantity,0)
     });
   }
 
   incrementQuantity(item: IProducts) {
     item.quantity++;
-    this.cartService.updateQuantity(item.id, item.quantity).subscribe();
+    this.totalValue+= item.quantity*this.totalValue
+    // this.cartService.updateQuantity(item.id, item.quantity).subscribe();
   }
   decremetQuantity(item: IProducts) {
     if (item.quantity > 1) {
       item.quantity--;
-      this.cartService.updateQuantity(item.id, item.quantity).subscribe();
+      // this.cartService.updateQuantity(item.id, item.quantity).subscribe();
     }
   }
   deleteCart(id: number) {
-    this.cartService.deleteCart(id).subscribe(() => this.getCart());
+    // this.cartService.deleteCart(id).subscribe(() => this.getCart());
   }
 }
--- front-end/src/app/core/interface/interface..ts
+++ front-end/src/app/core/interface/interface..ts
@@ -1,17 +1,22 @@
-export interface IProducts{
-    id:number,
-    name:string,
-    quantity:number,
-    description:string,
-    price:number,
-    status:number
+export interface IProducts {
+  id: number;
+  name: string;
+  quantity: number;
+  description: string;
+  price: number;
+  status: number;
 }
-
-
-export interface IUsers{
-    id:number,
-    name:string,
-    hierarchy:string,
-    email:string,
-    password:string
+export interface Icart {
+  productName: string;
+  productId: string;
+  quantity: number;
+  totalValue: number;
+}
+export interface IUsers {
+  id: number;
+  name: string;
+  hierarchy: string;
+  email: string;
+  password: string;
+  cart: Array<Icart>;
 }
